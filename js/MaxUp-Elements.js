40374a3fee65674d7a1559e1f0a2be0731554619
Sadegh <74778568+msghaderi77@users.noreply.github.com>
2020-12-11 19:02:12 +0330

    Update MaxUp-Elements.js

--- js/MaxUp-Elements.js
+++ js/MaxUp-Elements.js
@@ -1,19 +1,3 @@
-//
-// class BrandNav extends HTMLElement{
-//     constructor() {
-//         super();
-//         const shadow = this.attachShadow({mode: 'open'});
-//         let image = document.createElement('img');
-//         let hre = document.createElement('a');
-//         shadow.appendChild(hre);
-//         hre.appendChild(image);
-//         hre.setAttribute('href' , this.getAttribute('href'));
-//         image.setAttribute('src' , this.getAttribute('img'));
-//     }
-// }
-// customElements.define('nav-brand' , BrandNav );
-//
-
 $("input[type=text]").addClass('form');
 $("input[type=checkbox]").addClass('checkbox');
 
