9c3b74fc522a25227cb02d52b7a7d709da2895f3
Sadegh <msghaderi99@gmail.com>
2021-02-11 09:41:39 +0330

    bugs fixed

--- js/MaxUp.js
+++ js/MaxUp.js
@@ -24,67 +24,64 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     }
 
-
-
-    function RunXSelector(){
-        const xSelects = document.querySelectorAll('.xSelect');
-        xSelects.forEach(function (xSelect, index){
-
-            const xSelectLabel = xSelect.getElementsByClassName('xSelect__label');
-            const xSelectArrow = xSelectLabel[0].getElementsByClassName('xSelect__label__icon');
-            const xSelectOptionsBox = xSelect.getElementsByClassName('xSelect__options');
-            const xSelectSearch = xSelect.getElementsByClassName('xSelect__search');
-            const xSelectOptions = xSelectOptionsBox[0].querySelectorAll('.xSelect__option');
-
-            xSelectLabel[0].addEventListener('click',function (){
-                xSelectsToggleOptions(xSelectOptionsBox , xSelectArrow);
-            })
-            xSelectSearch[0].addEventListener('keydown' ,function (){
-                SearchXSelect(xSelectSearch , xSelectOptionsBox , xSelectOptions);
-            })
-            xSelectOptions.forEach(function (el){
-                const LabelName = el.getElementsByClassName('xSelect__option__name')[0];
-                LabelName.addEventListener('click',function (e){
-                    xSelectsToggleOptions(xSelectOptionsBox , xSelectArrow);
-                    xSelectLabel[0].getElementsByClassName('xSelect__label__title')[0].innerHTML = LabelName.innerText;
-                })
-            })
-        })
-    }
-
-    function SearchXSelect(input , ul , li) {
-
-        const InputValue = input[0].value.toLowerCase();
-        let filter, a, i, txtValue;
-        filter = input[0].value.toUpperCase();
-
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByClassName('xSelect__option__name')[0];
-            txtValue = a.textContent || a.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
-            }
-        }
-
-
-
-    }
-
-    function xSelectsToggleOptions(el,arrow){
-        el[0].classList.toggle('xToggle');
-        arrow[0].classList.toggle('xToggleArrow');
-    }
-
-    function SetDataCollapseOnNavToggle(){
-        navToggleBtn[0].setAttribute('data-collapse' , DataCollapse);
-    }
-
-
 });
 
+
+function RunXSelector(){
+    const xSelects = document.querySelectorAll('.xSelect');
+    xSelects.forEach(function (xSelect, index){
+
+        const xSelectLabel = xSelect.getElementsByClassName('xSelect__label');
+        const xSelectArrow = xSelectLabel[0].getElementsByClassName('xSelect__label__icon');
+        const xSelectOptionsBox = xSelect.getElementsByClassName('xSelect__options');
+        const xSelectSearch = xSelect.getElementsByClassName('xSelect__search');
+        const xSelectOptions = xSelectOptionsBox[0].querySelectorAll('.xSelect__option');
+
+        xSelectLabel[0].addEventListener('click',function (){
+            xSelectsToggleOptions(xSelectOptionsBox , xSelectArrow);
+        })
+        xSelectSearch[0].addEventListener('keydown' ,function (){
+            SearchXSelect(xSelectSearch , xSelectOptionsBox , xSelectOptions);
+        })
+        xSelectOptions.forEach(function (el){
+            const LabelName = el.getElementsByClassName('xSelect__option__name')[0];
+            LabelName.addEventListener('click',function (e){
+                xSelectsToggleOptions(xSelectOptionsBox , xSelectArrow);
+                xSelectLabel[0].getElementsByClassName('xSelect__label__title')[0].innerHTML = LabelName.innerText;
+            })
+        })
+    })
+}
+
+function SearchXSelect(input , ul , li) {
+
+    const InputValue = input[0].value.toLowerCase();
+    let filter, a, i, txtValue;
+    filter = input[0].value.toUpperCase();
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByClassName('xSelect__option__name')[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
+
+
+}
+
+function xSelectsToggleOptions(el,arrow){
+    el[0].classList.toggle('xToggle');
+    arrow[0].classList.toggle('xToggleArrow');
+}
+
+function SetDataCollapseOnNavToggle(){
+    navToggleBtn[0].setAttribute('data-collapse' , DataCollapse);
+}
 
 const PostFileProgress = (InputFile, ProgressBar, path) => {
     const formData = new FormData();
